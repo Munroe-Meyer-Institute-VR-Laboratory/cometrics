277e18d08f0150d077e2f97cbb7a63e01f1c811b
Walker Arce <wsarcera@gmail.com>
2021-08-27 16:58:50 -0500

    Created project and added utilities

---
+++ logger_util.py
@@ -0,0 +1,20 @@
+import sys
+from os import listdir, mkdir, path
+
+
+class Log(object):
+    def __init__(self):
+        if not path.isdir('./logs'):
+            mkdir('./logs')
+        self.log_num = len(listdir('./logs'))
+        self.orgstdout = sys.stdout
+        self.log_name = "./logs/log_" + str(self.log_num) + ".txt"
+
+    def write(self, msg):
+        self.log = open(self.log_name, "a")
+        self.orgstdout.write(msg)
+        self.log.write(msg)
+        self.log.close()
+
+    def flush(self):
+        pass
