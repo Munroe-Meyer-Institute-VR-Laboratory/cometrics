515960fc59550411868a589858c58b8e144f57e8
Walker Arce <wsarcera@gmail.com>
2021-08-27 17:00:32 -0500

    Update ka_ui.py
    Tied in logging and created self sizing window

--- ka_ui.py
+++ ka_ui.py
@@ -1,2 +1,15 @@
 from tkinter import *
 from tkinter import filedialog, messagebox
+from logger_util import *
+import datetime
+
+
+if __name__ == "__main__":
+    sys.stdout = Log()
+    sys.stderr = sys.stdout
+    print(datetime.datetime.now().strftime("%c"))
+    root = Tk()
+    root.config(bg="white", bd=-2)
+    pad = 3
+    root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth() - pad, root.winfo_screenheight() - pad))
+    root.title("KSA - KeyStroke Annotator")
