a12545936b9ac1480a6748ee04d6f0f78c3f0214
Walker Arce <wsarcera@gmail.com>
2021-08-28 11:54:45 -0500

    Update session_manager_ui.py
    - Patient information is laid out
    - Working on getting session file

--- session_manager_ui.py
+++ session_manager_ui.py
@@ -31,11 +31,61 @@ class SessionTimeFields:
 
 
 class PatientDataFields:
-    def __init__(self, parent):
-        self.frame = Frame(parent, width=350, height=500)
+    def __init__(self, parent, patient_file):
+        self.patient = PatientContainer(patient_file)
+        self.frame = Frame(parent, width=250, height=500)
         self.frame.pack(side=LEFT, padx=5, pady=5)
-        self.name_label = Label(self.frame, text="Patient Name")
-        self.name_label.pack(side=TOP, padx=5, pady=5)
+        # Create label canvas
+        self.label_canvas = Canvas(self.frame, width=250, height=500)
+        # Frame Information
+        self.frame_info = self.label_canvas.create_text(125, 10, text="Patient Information", anchor=CENTER,
+                                                        font=('Purisa', 12))
+        # Patient name field
+        self.name_label = self.label_canvas.create_text(5, 30, text="Name", anchor=NW,
+                                                        font=('Purisa', 10))
+        # Patient MRN field
+        self.mrn_label = self.label_canvas.create_text(5, 75, text="Medical Record Number", anchor=NW,
+                                                       font=('Purisa', 10))
+        # Session date field
+        # self.date_label = self.label_canvas.create_text(5, 120, text="Session Date",
+        #                                                 font=('Purisa', 10))
+        # Session start time field
+        # self.start_label = self.label_canvas.create_text(5, 165, text="Session Start Time", anchor=NW,
+        #                                                  font=('Purisa', 10))
+        # Session number field
+        # self.sess_number_label = self.label_canvas.create_text(5, 210, text="Session Number", anchor=NW,
+        #                                                        font=('Purisa', 10))
+        # Session location field
+        self.sess_loc_label = self.label_canvas.create_text(5, 120, text="Session Location", anchor=NW,
+                                                            font=('Purisa', 10))
+        # Assessment name field
+        self.assess_name_label = self.label_canvas.create_text(5, 165, text="Assessement Name", anchor=NW,
+                                                               font=('Purisa', 10))
+        # Condition name field
+        self.cond_name_label = self.label_canvas.create_text(5, 210, text="Condition Name", anchor=NW,
+                                                             font=('Purisa', 10))
+        # Primary therapist field
+        self.prim_ther_label = self.label_canvas.create_text(5, 255, text="Primary Therapist Name", anchor=NW,
+                                                             font=('Purisa', 10))
+        # Case manager field
+        self.case_mgr_label = self.label_canvas.create_text(5, 300, text="Case Manager Name", anchor=NW,
+                                                            font=('Purisa', 10))
+        # Session therapist field
+        self.sess_ther_label = self.label_canvas.create_text(5, 345, text="Session Therapist Name", anchor=NW,
+                                                             font=('Purisa', 10))
+        # Data recorder name
+        self.data_rec_label = self.label_canvas.create_text(5, 390, text="Data Recorder Name", anchor=NW,
+                                                            font=('Purisa', 10))
+        # Primary data field
+        self.prim_data_label = self.label_canvas.create_text(5, 435, text="Primary or Reliability Data", anchor=NW,
+                                                             font=('Purisa', 10))
+        # Setup input variables
+        self.patient_name_var = StringVar(self.frame)
+        if self.patient.name:
+            self.patient_name_var.set(self.patient.name)
+        self.name_field = Entry(self.frame, textvariable=self.patient_name_var)
+        self.name_field.place(x=15, y=50, width=220, anchor=NW)
+        self.label_canvas.pack()
 
 
 class EmpaticaDataFields:
@@ -45,7 +95,7 @@ class EmpaticaDataFields:
 
 
 class KeystrokeDataFields:
-    def __init__(self, parent):
+    def __init__(self, parent, keystroke_file):
         self.frame = Frame(parent, width=350, height=500)
         self.frame.pack(side=LEFT, padx=5, pady=5)
 
@@ -57,7 +107,8 @@ class PatientContainer:
         self.name = None
         self.medical_record_number = None
         self.age = None
-        self.update_fields(patient_file)
+        if patient_file:
+            self.update_fields(patient_file)
 
     def update_fields(self, filepath):
         f = open(filepath)
@@ -69,14 +120,30 @@ class PatientContainer:
 
 class SessionManagerWindow:
     def __init__(self, patient_file, keystroke_file):
+        parts = keystroke_file.split('/')[:-2]
+        self.session_files = []
+        self.session_file = None
+        self.session_dir = path.join(parts[0], parts[1], parts[2], 'sessions')
+        self.session_number = 1
+        self.get_session_file(self.session_dir)
         root = Tk()
         root.config(bg="white", bd=-2)
         pad = 3
         root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth() - pad, root.winfo_screenheight() - pad))
         root.title("KSA - KeyStroke Annotator")
         StaticImages(root)
-        PatientDataFields(root)
+        PatientDataFields(root, patient_file)
         EmpaticaDataFields(root)
-        KeystrokeDataFields(root)
-        SessionTimeFields(root)
+        KeystrokeDataFields(root, keystroke_file)
+        # SessionTimeFields(root)
         root.mainloop()
+
+    def get_session_file(self, directory):
+        if path.isdir(self.session_dir):
+            _, _, files = next(walk(directory))
+            for file in files:
+                if pathlib.Path(file).suffix == ".json":
+                    self.session_number += 1
+            self.session_file = open(path.join(directory, 'session_' + str(self.session_number) + '.json'))
+        else:
+            os.mkdir(directory)
