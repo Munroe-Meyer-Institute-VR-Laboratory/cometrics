234bf0dc4e2a3c6b45ca922d2e52047be0d1f457
Walker Arce <wsarcera@gmail.com>
2021-08-28 12:33:13 -0500

    Update session_manager_ui.py
    - Working on layout
    - Session number displayed at the bottom

--- session_manager_ui.py
+++ session_manager_ui.py
@@ -31,14 +31,14 @@ class SessionTimeFields:
 
 
 class PatientDataFields:
-    def __init__(self, parent, patient_file):
+    def __init__(self, parent, patient_file, session_number):
         self.patient = PatientContainer(patient_file)
-        self.frame = Frame(parent, width=250, height=500)
-        self.frame.pack(side=LEFT, padx=5, pady=5)
+        self.frame = Frame(parent, width=250, height=(parent.winfo_screenheight()-250))
+        self.frame.place(x=5, y=120)
         # Create label canvas
-        self.label_canvas = Canvas(self.frame, width=250, height=500)
+        self.label_canvas = Canvas(self.frame, width=250, height=(parent.winfo_screenheight()-250))
         # Frame Information
-        self.frame_info = self.label_canvas.create_text(125, 10, text="Patient Information", anchor=CENTER,
+        self.frame_info = self.label_canvas.create_text(125, 15, text="Patient Information", anchor=CENTER,
                                                         font=('Purisa', 12))
         # Patient name field
         self.name_label = self.label_canvas.create_text(5, 30, text="Name", anchor=NW,
@@ -46,15 +46,6 @@ class PatientDataFields:
         # Patient MRN field
         self.mrn_label = self.label_canvas.create_text(5, 75, text="Medical Record Number", anchor=NW,
                                                        font=('Purisa', 10))
-        # Session date field
-        # self.date_label = self.label_canvas.create_text(5, 120, text="Session Date",
-        #                                                 font=('Purisa', 10))
-        # Session start time field
-        # self.start_label = self.label_canvas.create_text(5, 165, text="Session Start Time", anchor=NW,
-        #                                                  font=('Purisa', 10))
-        # Session number field
-        # self.sess_number_label = self.label_canvas.create_text(5, 210, text="Session Number", anchor=NW,
-        #                                                        font=('Purisa', 10))
         # Session location field
         self.sess_loc_label = self.label_canvas.create_text(5, 120, text="Session Location", anchor=NW,
                                                             font=('Purisa', 10))
@@ -79,12 +70,59 @@ class PatientDataFields:
         # Primary data field
         self.prim_data_label = self.label_canvas.create_text(5, 435, text="Primary or Reliability Data", anchor=NW,
                                                              font=('Purisa', 10))
+        # Session date field
+        self.date_label = self.label_canvas.create_text(5, 480, text="Session Date", anchor=NW,
+                                                        font=('Purisa', 10))
+        # Session start time field
+        self.start_label = self.label_canvas.create_text(5, 525, text="Session Start Time", anchor=NW,
+                                                         font=('Purisa', 10))
+        # Session number field
+        self.sess_number_label = self.label_canvas.create_text(125, 580, text="Session Number " + str(session_number),
+                                                               anchor=CENTER,
+                                                               font=('Purisa', 12))
         # Setup input variables
         self.patient_name_var = StringVar(self.frame)
         if self.patient.name:
             self.patient_name_var.set(self.patient.name)
-        self.name_field = Entry(self.frame, textvariable=self.patient_name_var)
-        self.name_field.place(x=15, y=50, width=220, anchor=NW)
+        self.name_entry = Entry(self.frame, textvariable=self.patient_name_var)
+        self.name_entry.place(x=15, y=50, width=220, anchor=NW)
+
+        self.mrn_var = StringVar(self.frame)
+        self.mrn_entry = Entry(self.frame, textvariable=self.mrn_var)
+        self.mrn_entry.place(x=15, y=95, width=220, anchor=NW)
+
+        self.sess_loc_var = StringVar(self.frame)
+        self.sess_entry = Entry(self.frame, textvariable=self.sess_loc_var)
+        self.sess_entry.place(x=15, y=140, width=220, anchor=NW)
+
+        self.assess_name_var = StringVar(self.frame)
+        self.assess_name_entry = Entry(self.frame, textvariable=self.assess_name_var)
+        self.assess_name_entry.place(x=15, y=180, width=220, anchor=NW)
+
+        self.cond_name_var = StringVar(self.frame)
+        self.cond_name_entry = Entry(self.frame, textvariable=self.cond_name_var)
+        self.cond_name_entry.place(x=15, y=230, width=220, anchor=NW)
+
+        self.prim_ther_var = StringVar(self.frame)
+        self.prim_ther_entry = Entry(self.frame, textvariable=self.prim_ther_var)
+        self.prim_ther_entry.place(x=15, y=275, width=220, anchor=NW)
+
+        self.case_mgr_var = StringVar(self.frame)
+        self.case_mgr_entry = Entry(self.frame, textvariable=self.case_mgr_var)
+        self.case_mgr_entry.place(x=15, y=320, width=220, anchor=NW)
+
+        self.sess_ther_var = StringVar(self.frame)
+        self.sess_ther_entry = Entry(self.frame, textvariable=self.sess_ther_var)
+        self.sess_ther_entry.place(x=15, y=365, width=220, anchor=NW)
+
+        self.data_rec_var = StringVar(self.frame)
+        self.data_rec_entry = Entry(self.frame, textvariable=self.data_rec_var)
+        self.data_rec_entry.place(x=15, y=410, width=220, anchor=NW)
+
+        self.prim_data_var = StringVar(self.frame)
+        self.prim_data_entry = Entry(self.frame, textvariable=self.prim_data_var)
+        self.prim_data_entry.place(x=15, y=455, width=220, anchor=NW)
+
         self.label_canvas.pack()
 
 
@@ -132,9 +170,9 @@ class SessionManagerWindow:
         root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth() - pad, root.winfo_screenheight() - pad))
         root.title("KSA - KeyStroke Annotator")
         StaticImages(root)
-        PatientDataFields(root, patient_file)
-        EmpaticaDataFields(root)
-        KeystrokeDataFields(root, keystroke_file)
+        PatientDataFields(root, patient_file, self.session_number)
+        # EmpaticaDataFields(root)
+        # KeystrokeDataFields(root, keystroke_file)
         # SessionTimeFields(root)
         root.mainloop()
 
@@ -144,6 +182,6 @@ class SessionManagerWindow:
             for file in files:
                 if pathlib.Path(file).suffix == ".json":
                     self.session_number += 1
-            self.session_file = open(path.join(directory, 'session_' + str(self.session_number) + '.json'))
+            self.session_file = open(path.join(directory, 'session_' + str(self.session_number) + '.json'), 'w')
         else:
             os.mkdir(directory)
