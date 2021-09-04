45dcdde2800218801749a298b7b4d80ed9dfcba0
Walker Arce <wsarcera@gmail.com>
2021-09-04 13:34:07 -0500

    Update output_view_ui.py
    Graphs have default background color and frames working

--- output_view_ui.py
+++ output_view_ui.py
@@ -28,15 +28,36 @@ class OutputViews:
     E4_VIEW = 0
     VIDEO_VIEW = 1
     CAMERA_VIEW = 2
+    TACTOR_VIEW = 3
 
 
 class OutputViewPanel:
     def __init__(self, parent):
+        self.current_button = 0
+        self.view_buttons = []
+        self.view_frames = []
+
         self.frame = Frame(parent, width=700, height=(parent.winfo_screenheight() - 255))
         self.frame.place(x=780, y=95)
 
-        self.current_button = 0
-        self.view_buttons = []
+        e4_frame = Frame(parent, width=700, height=(parent.winfo_screenheight() - 280))
+        e4_frame.place(x=780, y=120)
+        self.view_frames.append(e4_frame)
+
+        video_frame = Frame(parent, width=700, height=(parent.winfo_screenheight() - 280))
+        test_label = Label(video_frame, text="Video Frame")
+        test_label.place(x=200, y=200)
+        self.view_frames.append(video_frame)
+
+        camera_frame = Frame(parent, width=700, height=(parent.winfo_screenheight() - 280))
+        test_label = Label(camera_frame, text="Camera Frame")
+        test_label.place(x=200, y=200)
+        self.view_frames.append(camera_frame)
+
+        tactor_frame = Frame(parent, width=700, height=(parent.winfo_screenheight() - 280))
+        test_label = Label(tactor_frame, text="Tactor Frame")
+        test_label.place(x=200, y=200)
+        self.view_frames.append(tactor_frame)
 
         e4_output_button = Button(self.frame, text="E4 View", command=self.switch_e4_frame, width=12)
         self.view_buttons.append(e4_output_button)
@@ -51,10 +72,17 @@ class OutputViewPanel:
         self.view_buttons.append(camera_view_button)
         self.view_buttons[2].place(x=184, y=0)
 
+        tactor_view_button = Button(self.frame, text="Tactor View", command=self.switch_tactor_frame, width=12)
+        self.view_buttons.append(tactor_view_button)
+        self.view_buttons[3].place(x=276, y=0)
+
         clean_view = Frame(self.frame, width=(700-(92*len(self.view_buttons))+2), height=25, bg='white')
         clean_view.place(x=(92*len(self.view_buttons))+2, y=0)
 
-        self.e4_view = ViewE4(self.frame)
+        self.e4_view = ViewE4(self.view_frames[OutputViews.E4_VIEW])
+
+    def switch_tactor_frame(self):
+        self.switch_frame(OutputViews.TACTOR_VIEW)
 
     def switch_camera_frame(self):
         self.switch_frame(OutputViews.CAMERA_VIEW)
@@ -72,13 +100,10 @@ class OutputViewPanel:
         :return:
         """
         self.view_buttons[self.current_button].config(relief=RAISED)
+        self.view_frames[self.current_button].place_forget()
         self.current_button = view
-        if view == OutputViews.E4_VIEW:
-            self.view_buttons[view].config(relief=SUNKEN)
-        elif view == OutputViews.VIDEO_VIEW:
-            self.view_buttons[view].config(relief=SUNKEN)
-        elif view == OutputViews.CAMERA_VIEW:
-            self.view_buttons[view].config(relief=SUNKEN)
+        self.view_buttons[view].config(relief=SUNKEN)
+        self.view_frames[view].place(x=780, y=120)
 
 
 class ViewE4:
@@ -97,81 +122,97 @@ class ViewE4:
         plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
         plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-        self.fig = Figure(figsize=(6, 1.75), dpi=100)
+        fig_color = '#%02x%02x%02x' % (240, 240, 237)
+
+        self.fig = Figure(figsize=(6.75, 1.75), dpi=100)
+        self.fig.patch.set_facecolor(fig_color)
         self.acc_plt = self.fig.add_subplot(111)
+        plt.gcf().subplots_adjust(bottom=0.15)
+        self.acc_plt.set_title("Accelerometer Readings")
+        self.acc_plt.legend(loc="upper left")
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)  # A tk.DrawingArea.
         self.canvas.draw()
-
-        self.fig1 = Figure(figsize=(6, 1.75), dpi=100)
-        self.bvp_plt = self.fig1.add_subplot(111)
-        self.canvas1 = FigureCanvasTkAgg(self.fig1, master=self.root)  # A tk.DrawingArea.
-        self.canvas1.draw()
-
-        self.fig2 = Figure(figsize=(6, 1.75), dpi=100)
-        self.gsr_plt = self.fig2.add_subplot(111)
-        self.canvas2 = FigureCanvasTkAgg(self.fig2, master=self.root)  # A tk.DrawingArea.
-        self.canvas2.draw()
-
-        self.ani = None
-        self.ani1 = None
-        self.ani2 = None
-
-    def start_plot(self, e4):
-        self.e4 = e4
-
         self.ani = animation.FuncAnimation(self.fig, self.animate, fargs=([]), interval=500)
         self.canvas.get_tk_widget().place(x=10, y=50)
+
+        self.fig1 = Figure(figsize=(6.75, 1.75), dpi=100)
+        self.fig1.patch.set_facecolor(fig_color)
+        self.bvp_plt = self.fig1.add_subplot(111)
+        plt.gcf().subplots_adjust(bottom=0.15)
+        self.bvp_plt.set_title("BVP Readings")
+        self.bvp_plt.legend(loc="upper left")
+        self.canvas1 = FigureCanvasTkAgg(self.fig1, master=self.root)  # A tk.DrawingArea.
+        self.canvas1.draw()
         self.ani1 = animation.FuncAnimation(self.fig1, self.bvp_animate, fargs=([]), interval=500)
         self.canvas1.get_tk_widget().place(x=10, y=225)
+
+        self.fig2 = Figure(figsize=(6.75, 1.75), dpi=100)
+        self.fig2.patch.set_facecolor(fig_color)
+        self.gsr_plt = self.fig2.add_subplot(111)
+        plt.gcf().subplots_adjust(bottom=0.15)
+        self.gsr_plt.set_title("GSR Readings")
+        self.gsr_plt.legend(loc="upper left")
+        self.canvas2 = FigureCanvasTkAgg(self.fig2, master=self.root)  # A tk.DrawingArea.
+        self.canvas2.draw()
         self.ani2 = animation.FuncAnimation(self.fig2, self.gsr_animate, fargs=([]), interval=500)
         self.canvas2.get_tk_widget().place(x=10, y=400)
 
+        self.streaming = False
+        self.e4 = None
+
+    def start_plot(self, e4):
+        self.e4 = e4
+        self.streaming = True
+
     def animate(self, e4):
-        if self.e4:
-            if self.e4.connected:
-                # Limit x and y lists to 20 items
-                x_ys = self.e4.acc_x[-100:]
-                y_ys = self.e4.acc_y[-100:]
-                z_ys = self.e4.acc_z[-100:]
-                xs = np.arange(0, len(self.e4.acc_x))
-                xs = xs[-100:]
+        if self.streaming:
+            if self.e4:
+                if self.e4.connected:
+                    # Limit x and y lists to 20 items
+                    x_ys = self.e4.acc_x[-100:]
+                    y_ys = self.e4.acc_y[-100:]
+                    z_ys = self.e4.acc_z[-100:]
+                    xs = np.arange(0, len(self.e4.acc_x))
+                    xs = xs[-100:]
 
-                # Draw x and y lists
-                self.acc_plt.clear()
-                self.acc_plt.plot(xs, x_ys, label="x-axis")
-                self.acc_plt.plot(xs, y_ys, label="y-axis")
-                self.acc_plt.plot(xs, z_ys, label="z-axis")
+                    # Draw x and y lists
+                    self.acc_plt.clear()
+                    self.acc_plt.plot(xs, x_ys, label="x-axis")
+                    self.acc_plt.plot(xs, y_ys, label="y-axis")
+                    self.acc_plt.plot(xs, z_ys, label="z-axis")
 
-                # Format plot
-                plt.gcf().subplots_adjust(bottom=0.15)
-                self.acc_plt.set_title("Accelerometer Readings")
-                self.acc_plt.legend(loc="upper left")
+                    # Format plot
+                    plt.gcf().subplots_adjust(bottom=0.15)
+                    self.acc_plt.set_title("Accelerometer Readings")
+                    self.acc_plt.legend(loc="upper left")
 
     def bvp_animate(self, e4):
-        if self.e4:
-            if self.e4.connected:
-                xs = np.arange(0, len(self.e4.bvp))
-                xs = xs[-100:]
-                x_ys = self.e4.bvp[-100:]
+        if self.streaming:
+            if self.e4:
+                if self.e4.connected:
+                    xs = np.arange(0, len(self.e4.bvp))
+                    xs = xs[-100:]
+                    x_ys = self.e4.bvp[-100:]
 
-                self.bvp_plt.clear()
-                self.bvp_plt.plot(xs, x_ys, label="bvp")
+                    self.bvp_plt.clear()
+                    self.bvp_plt.plot(xs, x_ys, label="bvp")
 
-                # Format plot
-                plt.gcf().subplots_adjust(bottom=0.15)
-                self.bvp_plt.set_title("BVP Readings")
-                self.bvp_plt.legend(loc="upper left")
+                    # Format plot
+                    plt.gcf().subplots_adjust(bottom=0.15)
+                    self.bvp_plt.set_title("BVP Readings")
+                    self.bvp_plt.legend(loc="upper left")
 
     def gsr_animate(self, e4):
-        if self.e4:
-            if self.e4.connected:
-                xs = np.arange(0, len(self.e4.gsr))
-                xs = xs[-100:]
-                x_ys = self.e4.gsr[-100:]
+        if self.streaming:
+            if self.e4:
+                if self.e4.connected:
+                    xs = np.arange(0, len(self.e4.gsr))
+                    xs = xs[-100:]
+                    x_ys = self.e4.gsr[-100:]
 
-                self.gsr_plt.clear()
-                self.gsr_plt.plot(xs, x_ys, label="gsr")
-                # Format plot
-                plt.gcf().subplots_adjust(bottom=0.15)
-                self.gsr_plt.set_title("GSR Readings")
-                self.gsr_plt.legend(loc="upper left")
+                    self.gsr_plt.clear()
+                    self.gsr_plt.plot(xs, x_ys, label="gsr")
+                    # Format plot
+                    plt.gcf().subplots_adjust(bottom=0.15)
+                    self.gsr_plt.set_title("GSR Readings")
+                    self.gsr_plt.legend(loc="upper left")
