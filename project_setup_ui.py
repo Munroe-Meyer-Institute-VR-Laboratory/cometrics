97a4b8d5a9e0d99acfc90fe29c79d208b728e0a2
Walker Arce <wsarcera@gmail.com>
2022-01-04 10:06:38 -0600

    Cleaned up treeview declaration and created phases dropdown

--- project_setup_ui.py
+++ project_setup_ui.py
@@ -26,22 +26,37 @@ class ProjectSetupWindow:
             self.window_height = int(self.window_height * 0.7)
         # Create global style
         _ = get_treeview_style()
-        # Define heading and column dicts, TODO: Move to config
-        heading_dict = {"#0": ["Recent Projects", 'w', 65, YES, 'w']}
-        column_dict = {"1": ["Col 1", 'c', 65, YES, 'c'],
-                       "2": ["Col 2", 'c', 65, YES, 'c']}
-        # Build treeviews, TODO: Parameterize the placements?
-        project_treeview, project_filescroll = build_treeview(self.main_root, ptp[0], ptp[1],
-                                                              int(self.window_height * 0.3),
-                                                              int(self.window_width * 0.45), heading_dict)
+        # Define heading and column dicts
+        project_heading_dict = {"#0": ["Recent Projects", 'w', 1, YES, 'w']}
+        patient_heading_dict = {"#0": ["Existing Patients", 'w', 1, YES, 'w']}
+        concern_heading_dict = {"#0": ["Presenting Concerns", 'w', 1, YES, 'w']}
+        # Build treeviews
+        self.project_treeview, self.project_filescroll = build_treeview(self.main_root, ptp[0], ptp[1],
+                                                                        int(self.window_height * 0.3),
+                                                                        int(self.window_width * 0.45),
+                                                                        project_heading_dict)
 
-        patient_treeview, patient_filescroll = build_treeview(self.main_root, ptp[0], ptp[1] + int(self.window_height * 0.35),
-                                                              int(self.window_height * 0.2),
-                                                              int(self.window_width * 0.45), heading_dict)
+        self.patient_treeview, self.patient_filescroll = build_treeview(self.main_root, ptp[0],
+                                                                        ptp[1] + int(self.window_height * 0.35),
+                                                                        int(self.window_height * 0.2),
+                                                                        int(self.window_width * 0.45),
+                                                                        patient_heading_dict)
 
-        concern_treeview, concern_filescroll = build_treeview(self.main_root, ptp[0], ptp[1] + int(self.window_height * 0.35) + int(self.window_height * 0.25),
-                                                              int(self.window_height * 0.2),
-                                                              int(self.window_width * 0.45), heading_dict)
+        self.concern_treeview, self.concern_filescroll = build_treeview(self.main_root, ptp[0],
+                                                                        ptp[1] + int(self.window_height * 0.35) + int(
+                                                                            self.window_height * 0.25),
+                                                                        int(self.window_height * 0.2),
+                                                                        int(self.window_width * 0.45),
+                                                                        concern_heading_dict)
+        # Get phases and create dropbox
+        self.phases = config.get_phases()
+        if self.phases:
+            self.phases_var = StringVar(self.main_root)
+            self.phases_var.set("Select a Phase")
+            self.phases_menu = OptionMenu(self.main_root, self.phases_var, *self.phases)
+            self.phases_menu.place(x=ptp[0],
+                                   y=ptp[1] + int(self.window_height * 0.35) + int(self.window_height * 0.25) + int(self.window_height * 0.22),
+                                   width=int(self.window_width * 0.2))
         # Create window geometry, center, and display
         self.main_root.geometry("{0}x{1}+0+0".format(self.window_width, self.window_height))
         center(self.main_root)
