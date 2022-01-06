d2011db6221103da59d2f86a661f56d2e1c5098d
Walker Arce <wsarcera@gmail.com>
2022-01-05 21:32:54 -0600

    Selecting concern enables the phase selection

--- project_setup_ui.py
+++ project_setup_ui.py
@@ -6,22 +6,23 @@ import yaml
 from config_utils import ConfigUtils
 from logger_util import *
 from tkinter_utils import center, get_display_size, get_treeview_style, build_treeview, EntryPopup, select_focus
-from ui_params import project_treeview_params as ptp, treeview_tags
+from ui_params import project_treeview_params as ptp, treeview_tags, ksf_distance, window_ratio
 
 
 class ProjectSetupWindow:
     def __init__(self, config):
         self.config = config
-        # Window setup
+        # region Window setup
         if len(config.get_screen_size()) == 2:
             self.main_root = Tk()
-            self.window_height = int(config.get_screen_size()[0] * 0.7)
-            self.window_width = int(config.get_screen_size()[1] * 0.7)
+            self.window_height = int(config.get_screen_size()[0] * window_ratio)
+            self.window_width = int(config.get_screen_size()[1] * window_ratio)
         else:
             self.main_root, self.window_height, self.window_width = get_display_size()
             config.set_screen_size(self.window_height, self.window_width)
-            self.window_width = int(self.window_width * 0.7)
-            self.window_height = int(self.window_height * 0.7)
+            self.window_width = int(self.window_width * window_ratio)
+            self.window_height = int(self.window_height * window_ratio)
+        # endregion
         # Create Project Setup label
         self.project_setup_label = Label(self.main_root, text="Project Setup", font=('Purisa', 15, 'bold'))
         self.project_setup_label.place(x=ptp[0], y=ptp[1] / 2, anchor=W)
@@ -62,26 +63,28 @@ class ProjectSetupWindow:
         if self.phases:
             self.phases_var = StringVar(self.main_root)
             self.phases_var.set("Select a Phase")
+            self.phases_var.trace("w", self.phase_callback)
             self.phases_menu = OptionMenu(self.main_root, self.phases_var, *self.phases)
             self.phases_menu.place(x=ptp[0],
                                    y=ptp[1] + int(self.window_height * 0.35) + int(self.window_height * 0.25) + int(
                                        self.window_height * 0.22),
                                    width=int(self.window_width * 0.2))
+            self.phases_menu.config(state='disabled')
         # Create KSF label
         self.ksf_setup_label = Label(self.main_root, text="Keystroke File Setup", font=('Purisa', 15, 'bold'))
-        self.ksf_setup_label.place(x=ptp[0] + ptp[3] * 1.8, y=ptp[1] / 2, anchor=W)
-        self.ksf_path = Label(self.main_root, text="Select Condition to Load", font=('Purisa', 11, 'italic'),
+        self.ksf_setup_label.place(x=ptp[0] + ptp[3] * ksf_distance, y=ptp[1] / 2, anchor=W)
+        self.ksf_path = Label(self.main_root, text="Select Condition and Phase to Load", font=('Purisa', 11, 'italic'),
                               bg='white', width=35, anchor='w')
-        self.ksf_path.place(x=ptp[0] + ptp[3] * 1.8, y=ptp[1])
+        self.ksf_path.place(x=ptp[0] + ptp[3] * ksf_distance, y=ptp[1])
         self.ksf_import = Button(self.main_root, text="Import", font=('Purisa', 11), width=10)
-        self.ksf_import.place(x=330 + ptp[0] + ptp[3] * 1.8, y=ptp[1] - 4)
+        self.ksf_import.place(x=330 + ptp[0] + ptp[3] * ksf_distance, y=ptp[1] - 4)
         # Define frequency and duration key headers
         freq_heading_dict = {"#0": ["Frequency Key", 'w', 1, YES, 'w']}
         dur_heading_dict = {"#0": ["Duration Key", 'w', 1, YES, 'w']}
         key_column_dict = {"1": ["Tag", 'w', 1, YES, 'w']}
         key_treeview_height = int(self.window_height * 0.2)
         self.frequency_key_treeview, self.frequency_key_filescroll = build_treeview(self.main_root,
-                                                                                    ptp[0] + ptp[3] * 1.8,
+                                                                                    ptp[0] + ptp[3] * ksf_distance,
                                                                                     ptp[1] + (self.window_height * 0.1),
                                                                                     key_treeview_height,
                                                                                     treeview_width,
@@ -89,7 +92,7 @@ class ProjectSetupWindow:
                                                                                     key_column_dict)
 
         self.duration_key_treeview, self.duration_key_filescroll = build_treeview(self.main_root,
-                                                                                  ptp[0] + ptp[3] * 1.8,
+                                                                                  ptp[0] + ptp[3] * ksf_distance,
                                                                                   ptp[1] + (self.window_height * 0.15) + (self.window_height * 0.2),
                                                                                   key_treeview_height,
                                                                                   treeview_width,
@@ -239,6 +242,8 @@ class ProjectSetupWindow:
         selection = self.project_treeview.identify_row(event.y)
         if selection == '0':
             self.create_new_concern()
+        else:
+            self.phases_menu.config(state='active')
 
     def create_new_concern(self):
         EntryPopup(self, self.main_root, "Enter New Concern", 2)
@@ -256,9 +261,17 @@ class ProjectSetupWindow:
         self.read_concern_file()
     # endregion
 
-    # region KSF UI Controls
-    # def load_concern_ksf(self):
+    # region Phase UI Controls
+    def phase_callback(self, *args):
+        print(f"the variable has changed to '{self.phases_var.get()}'")
+    # endregion
 
+    # region KSF UI Controls
+    def load_concern_ksf(self):
+        print("Load concern ksf")
+
+    def import_concern_ksf(self):
+        print("Import concern ksf")
     # endregion
 
 
--- ui_params.py
+++ ui_params.py
@@ -1,3 +1,5 @@
 # x, y, height, width
 project_treeview_params = [40, 60, 400, 300]
 treeview_tags = ['odd', 'even', 'header']
+ksf_distance = 1.75
+window_ratio = 0.7
